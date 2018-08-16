312339653683c08ac234509b53bb541b15ca3d94
Kevin K <doilerjar@outlook.com>
2018-08-16 00:30:10 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,6 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute={})
-
     sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute.keys[0]} = '#{attribute[keys[0]]}'"
     binding.pry
     DB[:conn].execute(sql)
